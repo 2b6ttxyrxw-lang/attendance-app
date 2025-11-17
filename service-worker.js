1abecb245725bce088eb3772f3ac3736fd60be75
2b6ttxyrxw-lang <2b6ttxyrxw@privaterelay.appleid.com>
2025-11-17 19:58:50 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "attendance-cache-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => res))
+  );
+});
